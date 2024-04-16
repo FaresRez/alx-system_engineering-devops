0bf152ef0ff6ffa108fecdb3d11719eb55708864
FaresRez <fers.rez16@gmail.com>
2024-04-16 07:54:20 -0700

    Create a pupple that create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#this pupple create a file with some ressources
+
+    file {'/tmp/school':
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+
+}
+
